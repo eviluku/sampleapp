61780c5ca184cd4a4f89bf91ca450171e1b8e302
Uku Loskit <ukuloskit@gmail.com>
2015-11-17 10:25:27 +0200

    Initial Commit

---
+++ sampleapp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  sampleapp
+//
+//  Created by Uku on 17/11/15.
+//  Copyright © 2015 Uku. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
